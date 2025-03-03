747d8bd3b622f0f299e3c96efa83da5da644fd93
Carolina Jung do Amaral <carolina.amaral@pmpa.ad>
2025-03-03 18:12:26 -0300

    feature: uso de prefetch para carregar dados de filmes ao passar mouse sob card

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Provider } from "react-redux";
 import store from "./store";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient({
+export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 30 * 1000,
--- src/views/movies/Movies.tsx
+++ src/views/movies/Movies.tsx
@@ -1,10 +1,22 @@
+import { queryClient } from "../../App";
 import { MovieItem } from "../../components/movieItem/MovieItem";
 import { useMovies } from "../../hooks/useMovies";
 import { Movie } from "../../interfaces/movie.interface";
+import { getMovieDetails } from "../../services/movies.service";
 import style from "./Movies.module.css";
 
 export function Movies() {
   const { data: movies, isLoading } = useMovies();
+
+  async function prefetchMovie(movieId: number) {
+    await queryClient.prefetchQuery({
+      queryKey: ["movies", movieId],
+      queryFn: async () => {
+        const { data } = await getMovieDetails(movieId);
+        return data;
+      },
+    });
+  }
 
   if (isLoading) {
     return <p>Carregando...</p>;
@@ -15,7 +27,12 @@ export function Movies() {
       <h1>Filmes populares</h1>
       <div className={style.moviesList}>
         {movies.map((movie: Movie) => (
-          <MovieItem key={movie.id} movie={movie} />
+          <div
+            key={movie.id}
+            onMouseEnter={async () => await prefetchMovie(movie.id)}
+          >
+            <MovieItem movie={movie} />
+          </div>
         ))}
       </div>
     </section>
